9aeb2c7997c69e0d126e7f5f29aa999fd8ff37f0
jahill <jahill@groupon.com>
2023-02-10 11:00:35 +0000

    feat: use component property for tagName

--- src/index.tsx
+++ src/index.tsx
@@ -22,4 +22,4 @@ import { InfoArea } from '@/components/infoArea.component';
  *
  * -------------------------------- */
 
-define('info-area', () => InfoArea, { attributes: InfoArea.attributes });
+define(InfoArea.tagName, () => InfoArea, { attributes: InfoArea.attributes });
